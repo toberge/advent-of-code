c5c8caacb37978eab3d1316256e2753ee310ef57
Tore Bergebakken <toberge@stud.ntnu.no>
2021-12-08 09:53:04 +0100

    2021: Day 6, efficiently

--- 2021/day6/lanternfish.py
+++ 2021/day6/lanternfish.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 
 def update(count):
@@ -8,19 +9,39 @@ def update(count):
         return (count - 1) % 7
 
 
-def simulate(initial_fishes, days):
+def simulate_naive(initial_fishes, days):
     """Naive solution that works for Part 1"""
     fishes = initial_fishes[:]
-    next_spawnage = 0
-    for i in range(days):
-        if i % 10 == 0:
-            print(i, len(fishes))
+    next_spawnage = sum(fish == 0 for fish in fishes)
+    for _ in range(days):
         fishes = [update(fish) for fish in fishes]
         if next_spawnage > 0:
             fishes += [8] * next_spawnage
         next_spawnage = sum(fish == 0 for fish in fishes)
 
     return len(fishes)
+
+
+def simulate(initial_fishes, days):
+    """Efficient solution that only keeps track of the number of fishes per timer value"""
+    fishes = defaultdict(int)
+    for days_remaining in initial_fishes:
+        fishes[days_remaining] += 1
+
+    for _ in range(days):
+        # Remember the number of new fishes to spawn
+        newborns = fishes[0]
+        next_fishes = defaultdict(int)
+        # Age the existing fishes
+        for i in range(7):
+            next_fishes[(i - 1) % 7] = fishes[i]
+        next_fishes[6] += fishes[7]
+        next_fishes[7] = fishes[8]
+        # Spawn new fishes
+        next_fishes[8] = newborns
+        fishes = next_fishes
+
+    return sum(fishes.values())
 
 
 def main():
