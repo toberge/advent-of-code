856bb76327bd0b9a2ba3c67df0a78a562de80961
Tore Bergebakken <toberge@stud.ntnu.no>
2021-12-13 17:45:37 +0100

    2021: WIP day 13

---
+++ 2021/day13/paper_folding.py
@@ -0,0 +1,66 @@
+import numpy as np
+import sys
+from itertools import takewhile, product
+
+lines = sys.stdin.readlines()
+dots = [
+    tuple(map(int, line[:-1].split(",")))
+    for line in list(takewhile(lambda l: l != "\n", lines))
+]
+
+shape = (max(dots, key=lambda x: x[1])[1] + 1, max(dots, key=lambda x: x[0])[0] + 1)
+paper = np.ndarray(shape, dtype=bool)
+paper[:, :] = 0
+
+print(shape)
+
+
+def show(paper: np.ndarray):
+    yy, xx = paper.shape
+    for y in range(yy):
+        for x in range(xx):
+            print("#" if paper[y, x] else ".", end="")
+        print()
+
+
+for x, y in dots:
+    paper[y, x] = True
+
+
+# show(paper)
+# print(paper.shape)
+
+
+def flip(paper, index, axis=0):
+    size = index
+    end = max(paper.shape[axis] - 1, 2 * size)
+    padding = 2 * size - (paper.shape[axis] - (paper.shape[axis] % 2))
+    print(size, end, padding)
+
+    if axis == 0:
+        show(paper[:size, :])
+        print("aaa")
+        show(
+            np.pad(
+                np.flip(paper[size:, :], axis)[: end - size, :], ((0, padding), (0, 0))
+            )
+        )
+        print("aaa")
+        return paper[:size, :] | np.pad(
+            np.flip(paper[size:, :], axis)[: end - size, :], ((0, padding), (0, 0))
+        )
+    return paper[:, :size] | np.pad(
+        np.flip(paper[:, size:], axis)[:, : end - size], ((0, 0), (0, padding))
+    )
+
+
+it = iter(lines)
+line = next(it)
+while line != "\n":
+    line = next(it)
+line = next(it)
+command = line.replace("fold along ", "")[:-1].split("=")
+print(command)
+paper = flip(paper, int(command[1]), axis=int(command[0] == "x"))
+show(paper)
+print(np.count_nonzero(paper))
